a560930d303e20e74e23cff0dcc79a8d69a9c433
afifa.julia <vdr_ajulia@airasia.com>
2026-07-08 03:18:38 +0800

    feat: configure PostgreSQL datasource

--- src/main/java/com/collabera/library_application/LibraryApplication.java
+++ src/main/java/com/collabera/library_application/LibraryApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LibraryApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Starting Library Management Application...");
 		SpringApplication.run(LibraryApplication.class, args);
 	}
 
